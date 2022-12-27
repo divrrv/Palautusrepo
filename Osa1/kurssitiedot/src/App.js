858753de97b9fa9c576d31b14dfd9c30a831cdb1
divrrv <112497215+divrrv@users.noreply.github.com>
2022-12-27 17:42:47 +0200

    Osa1-1.2

--- Osa1/kurssitiedot/src/App.js
+++ Osa1/kurssitiedot/src/App.js
@@ -12,14 +12,24 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
+      <Part
+        part={props.part1} exercises={props.exercises1}
+      />
+      <Part
+        part={props.part2} exercises={props.exercises2}
+      />
+      <Part
+        part={props.part3} exercises={props.exercises3}
+      />
+    </div>
+  )
+}
+//Name and number of the part 
+const Part = (props) => {
+  return (
+    <div>
       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
+        {props.part} {props.exercises}
       </p>
     </div>
   )
