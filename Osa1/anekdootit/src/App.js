0d306dfa9e65a6a766377925ac6f614cc06e1509
divrrv <112497215+divrrv@users.noreply.github.com>
2022-12-29 14:29:03 +0200

    Osa1-1.12

---
+++ Osa1/anekdootit/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const length = anecdotes.length
+  const randomize = () => setSelected(Math.floor(Math.random()*length))
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br/>
+      <Button handleClick={randomize} text="next anecdote"/>
+    </div>
+  )
+}
+
+export default App
--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 
 const StatisticsLine = ({ text, value }) => {
-  return(
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td>
-  </tr>
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -30,12 +30,12 @@ const Statistics = ({ good, neutral, bad }) => {
       <div>
         <table>
           <tbody>
-          <StatisticsLine text="good " value={good}/>
-          <StatisticsLine text="neutral" value={neutral}/>
-          <StatisticsLine text="bad " value={bad}/>
-          <StatisticsLine text="all " value={all}/>
-          <StatisticsLine text="average " value={average}/>
-          <StatisticsLine text="positive " value={positive}/>
+            <StatisticsLine text="good " value={good} />
+            <StatisticsLine text="neutral" value={neutral} />
+            <StatisticsLine text="bad " value={bad} />
+            <StatisticsLine text="all " value={all} />
+            <StatisticsLine text="average " value={average} />
+            <StatisticsLine text="positive " value={positive} />
           </tbody>
         </table>
       </div>
