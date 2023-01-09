d545aec2f4e08082d989f94f9132c2c2951762e3
divrrv <112497215+divrrv@users.noreply.github.com>
2023-01-09 12:39:31 +0200

    Osa2-2.2

--- Osa2/kurssitiedot/src/App.js
+++ Osa2/kurssitiedot/src/App.js
@@ -1,36 +1,50 @@
-const Course =({course}) => (
+const Course = ({ course }) => (
   <div>
     <Header course={course.name} />
     <Content parts={course.parts} />
+    <Total course={course} />
   </div>
-  )
+)
 
 //Course name
-const Header = ({course}) => {
+const Header = ({ course }) => {
   return (
-      <h1>
-        {course}
-      </h1>
+    <h1>
+      {course}
+    </h1>
   )
 }
 //Parts and number of exercises in them
-const Content = ({parts}) => {
+const Content = ({ parts }) => {
   return (
     <div>
       {parts.map(part =>
         <Part key={part.id} part={part} />
-        )}
+      )}
     </div>
   )
 }
 //Name and number of the part 
-const Part = ({part}) => {
+const Part = ({ part }) => {
   return (
-      <p>
-        {part.name} {part.exercises}
-      </p>
+    <p>
+      {part.name} {part.exercises}
+    </p>
   )
 }
+//Sum exercises
+const Sum = ({ parts }) =>
+  <>
+    {parts.reduce((sum, part) =>
+      sum + part.exercises, 0)}
+  </>
+
+//Total number of exercises
+const Total = ({ course }) => 
+    <div>
+      <h4>total of <Sum parts={course.parts} /> exercises </h4>
+    </div>
+
 
 const App = () => {
   const course = {
