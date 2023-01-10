fcde1efe13d64d439ce7b9931e00707c426e9681
divrrv <112497215+divrrv@users.noreply.github.com>
2023-01-10 13:52:06 +0200

    Osa2-2.9

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -3,11 +3,18 @@ import Person from './components/Person'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
 
   const [newNumber, setNewNumbers] = useState([])
 
   const [newName, setNewName] = useState('')
+
+  const [nameFilter, setNameFilter] = useState('')
 
   const savePerson = (event) => {
     const personObject = {
@@ -37,33 +44,42 @@ const App = () => {
     setNewNumbers(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setNameFilter(event.target.value)
+  }
+
   return (
     <div>
       <h1> Phonebook </h1>
+      <p>filter shown with
+        <input value={nameFilter} onChange={handleFilterChange} />
+      </p>
+      <h2> add a new </h2>
       <form onSubmit={addPerson}>
         <p>
-        name:
-        <input value={newName} onChange={handlePersonChange}
-        />
+          name:
+          <input value={newName} onChange={handlePersonChange}
+          />
         </p>
         <p>
-        number:
-        <input value={newNumber} onChange={handleNumberChange}
-        />
+          number:
+          <input value={newNumber} onChange={handleNumberChange}
+          />
         </p>
         <button type="submit"> add </button>
       </form>
       <h2> Numbers </h2>
       <div>
-        {persons.map(person =>
-          <Person
-            key={person.id}
-            person={person}
-          />)}
+        {persons.filter(person =>
+          (person.name.toLowerCase().includes(nameFilter.toLowerCase())))
+          .map(person =>
+            <Person
+              key={person.id}
+              person={person}
+            />)}
       </div>
     </div>
   )
-
 }
 
 export default App
